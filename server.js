7ab5e6a649abee4da657bc83aa60b3c268b7a268
IshikaRedhu <114400978+IshikaRedhu@users.noreply.github.com>
2023-08-07 20:54:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.get("/", (req , res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    
+    socket.on("message", (message,username) => {
+        io.emit("createMessage", message,username);
+    });
+});
+
+server.listen(3030);
